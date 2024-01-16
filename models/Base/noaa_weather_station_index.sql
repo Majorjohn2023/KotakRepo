02cb3254d4390d51a09ddda04bd5dc226642af0c
root <root@ip-10-0-102-94.ec2.internal>
2024-01-16 14:06:19 +0000

    sds

--- models/Base/noaa_weather_station_index.sql
+++ models/Base/noaa_weather_station_index.sql
@@ -1,11 +1,11 @@
--- {{ config(materialized='view') }}
+{{ config(materialized='view') }}
 
--- select
---     NOAA_WEATHER_STATION_ID,
---     NOAA_WEATHER_STATION_NAME,
---     COUNTRY_GEO_ID,
---     STATE_GEO_ID,
---     LATITUDE,
---     LONGITUDE,
---     ELEVATION
--- from {{ source('environmental_data', 'NOAA_WEATHER_STATION_INDEX') }}
+select
+    NOAA_WEATHER_STATION_ID,
+    NOAA_WEATHER_STATION_NAME,
+    COUNTRY_GEO_ID,
+    STATE_GEO_ID,
+    LATITUDE,
+    LONGITUDE,
+    ELEVATION
+from {{ source('environmental_data', 'NOAA_WEATHER_STATION_INDEX') }}
