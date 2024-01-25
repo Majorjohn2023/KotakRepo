49a8dd4760035dd0eef8fbae302046510caf7042
root <root@ip-10-0-101-212.ec2.internal>
2024-01-25 13:40:08 +0000

    sd

--- models/Base/carbon_credit_purchase_attributes.sql
+++ models/Base/carbon_credit_purchase_attributes.sql
@@ -1,9 +1,14 @@
-{{
-    config(
-        materialized = 'view', 
-        tags = ["daily"]
-    )
-}}
+-- models/carbon/carbon_credit_purchase_attributes.sql
 
+{{ config(materialized='view') }}
+{{ config(
+    tags=["daily", "analytics"]
+) }}
 
-select 1
+select
+    VARIABLE,
+    VARIABLE_NAME,
+    GROUPING_TYPE,
+    ENTITY,
+    MEASURE_TYPE
+from {{ source('environmental_data', 'CARBON_CREDIT_PURCHASE_ATTRIBUTES') }}
